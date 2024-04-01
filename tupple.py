e0c5b9439325927beea1484da210b5d48fef1825
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-04-01 17:54:47 +0300

    Create tupple.py

---
+++ tupple.py
@@ -0,0 +1,16 @@
+tuple1 = (1,2,3,4,5,6)
+print(tuple1[2:4])
+
+numbers = (10,15,24,10,29,34,45,10)
+x=numbers.count(10)
+print(x)
+
+x=y=z=100
+print(x)
+print(y)
+print(z)
+
+a,b,list1 = 10,20,[1,2,3]
+print(a)
+print(b)
+print(list1 )
