88f4cfb1e5e4b1a0e9323fadd260899421f20675
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-04-01 17:55:49 +0300

    Create tupples.py

---
+++ tupples.py
@@ -0,0 +1,23 @@
+import turtle
+
+t= turtle.Turtle ()
+s = turtle.screen()
+s.bgcolor("black")
+t.pencolor("green")
+
+
+a=0
+b=0
+while True:
+    t.forward(a)
+    t.right(b)
+    a+=3
+    b+=1
+
+    if b==320
+      break
+    t.hideturtle()
+
+    turtle.done()
+
+
